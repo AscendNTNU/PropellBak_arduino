63ca8d3f7e108d529455b10133e72e6355430b03
Sondre H. Rognlien <sondreholth98@gmail.com>
2020-04-14 11:49:36 +0200

    fikset slider_counter

--- gimbal.ino
+++ gimbal.ino
@@ -7,7 +7,7 @@ void feedback360()
   
   feedback = measured_time/1100.0;                       //Control signal with duty cycle between 0.029(0 deg) - 0.971(359 deg) or 2.9 - 97.1%
   feedback = (feedback - 0.029)* 359/0.942;              //if feedback = 0.971, feedback - 0.029 = 0.942, which divided by 0.942 times 359 = 359 
-                                                         //This formula transforms from duty cycle to degrees between(0 - 359)
+                                                         //This formula transforms from duty_cycle to degrees between(0 - 359)
   
   uint8_t offset = 0;
   float error;
@@ -26,9 +26,17 @@ void feedback360()
     }
   }
     else{
-    slider = (1500 + (error * slider_Kp)) + offset;
+    slider = 1500 + (error * slider_Kp) + offset;
   }
 
+  if (prev_feedback - feedback > 350){
+    slider_counter -= 1;
+  }
+
+  else if (prev_feedback - feedback < 350){
+    slider_counter += 1;
+  }
+  
   prev_feedback = feedback;
 }    
 
